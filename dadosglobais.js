30bca54c9e7679e188ef27cabae93fafde675dfa
kraemerpedro <125660227+kraemerpedro@users.noreply.github.com>
2024-10-15 16:59:43 -0300

    Add files via upload

--- dadosglobais.js
+++ dadosglobais.js
@@ -5,7 +5,16 @@ async function visualizarinformacoesglobais()
 {
 const res =await fetch(url);
 const dados =await res.json();
-console.log(dados);
+const paragrafo=document.createElement ('p');
+paragrafo.classList.add('graficos-container__texto');
+paragrafo.innerHTML=`voce sabia que o mundo tem aproximadamente 
+${dados.total_pessoas_mundo}de pessoas e que aproximadamente
+ ${dados.total_pessoas_conectadas} estao conectadas em alguma rede social e passam em media
+  ${dados.tempo_medio}horas conectadas`
+  
+  const container = document.getElementById('graficos-container');
+  container.appendChild(paragrafo);
 
+  console.log(paragrafo)
 }
 visualizarinformacoesglobais()
